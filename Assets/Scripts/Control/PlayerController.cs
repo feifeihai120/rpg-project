640a196795d22256b329d188c9273d8a0a2fe618
abaddon-dev <pawel.dwornicki86@gmail.com>
2022-03-12 19:50:52 +0100

    Added enemy AI: calculating chase distance and attack.

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -23,10 +23,15 @@ namespace RPG.Control
             {
                 CombatTarget target = hit.transform.GetComponent<CombatTarget>();
                 if (target == null) continue;
+                
+                if (!GetComponent<Fighter>().CanAttack(target.gameObject))
+                {
+                    continue;
+                }
 
                 if(Input.GetMouseButtonDown(0))
                 {
-                    GetComponent<Fighter>().Attack(target);
+                    GetComponent<Fighter>().Attack(target.gameObject);   
                 }
                 return true;
             }
